4fa43a5508be8453ee120a928c290d97cc446a8a
Tom Thorogood <tomthorogood@outlook.com>
2021-02-14 02:33:01 +0000

    Created ClassicVigSquare

---
+++ src/key/vigenere_square/classic_vig_square.rs
@@ -0,0 +1,48 @@
+use key::Key;
+use crate::key::key;
+use super::vig_square::{VigSquare, vig_square_to_string};
+
+pub struct ClassicVigSquare {
+    square: Vec<Vec<i16>>,
+    inverse: Vec<Vec<i16>>
+}
+
+impl VigSquare for ClassicVigSquare {
+    fn init_squares(&mut self) {
+        for row in 0..26 {
+            let mut idx: usize = 0;
+            for col in row..26 {
+                self.inverse[row][col] = idx as i16;
+                self.square[row][idx] = col as i16;
+                idx += 1;
+            }
+            for col in 0..row {
+                self.inverse[row][col] = idx as i16;
+                self.square[row][idx] = col as i16;
+                idx += 1;
+            }
+        }
+    }
+
+    fn encrypt(&self, x: i16, y: i16) -> i16 {
+        self.square[x as usize][y as usize]
+    }
+    fn decrypt(&self, x: i16, y: i16) -> i16 {
+        self.inverse[x as usize][y as usize]
+    }
+}
+
+impl Key for ClassicVigSquare {
+    fn to_string(&self) -> String {
+        vig_square_to_string(&self.square, 26, 26)
+    }
+    fn new() -> ClassicVigSquare {
+        let my_square = vec![vec![0; 26]; 26];
+        let mut vig_square = ClassicVigSquare {
+            square: my_square.clone(),
+            inverse: my_square
+        };
+        vig_square.init_squares();
+        vig_square
+    }
+}
---
+++ src/key/vigenere_square/vig_square.rs
@@ -0,0 +1,33 @@
+pub trait VigSquare {
+    fn init_squares(&mut self);
+
+    fn encrypt(&self, x: i16, y: i16) -> i16;
+    fn decrypt(&self, x: i16, y: i16) -> i16;
+}
+
+pub fn vig_square_to_string(square: &Vec<Vec<i16>>, max_y: usize, max_x: usize) -> String {
+    let mut square_as_string = String::new();
+
+    // left padding
+    square_as_string.push_str("  | ");
+
+    // print headers
+    for i in 0..26 {
+        square_as_string.push((i as u8+65) as char);
+        square_as_string.push(' ');
+    }
+    square_as_string.push_str("\n==|====================================================\n");
+
+    // main square
+    for y in 0..max_y {
+        square_as_string.push((y as u8+65) as char);
+        square_as_string.push_str(" | ");
+        for x in 0..max_x {
+            square_as_string.push((square[y][x] as u8+65) as char);
+            square_as_string.push(' ');
+        }
+        square_as_string.push('\n')
+    }
+
+    square_as_string
+}
