22c72e7454dc5a5c4140a3c81df9d50660934386
Tom Thorogood <tomthorogood@outlook.com>
2021-02-18 19:02:50 +0000

    Implemented Morse code

---
+++ src/cipher/monoalph/morse.rs
@@ -0,0 +1,77 @@
+use crate::cipher::cipher::Asymmetric;
+use crate::lang::Language;
+
+const LETTERS: [char; 37] = [
+    ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+    's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+];
+const MORSE: [&str; 37] = [
+    "/", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
+    "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--",
+    "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
+    "-----",
+];
+
+pub struct Morse {}
+
+impl Asymmetric for Morse {
+    fn encrypt(&self, _language: &Language, msg: &String) -> String {
+        msg
+            .to_lowercase()
+            .chars()
+            .filter(|c| LETTERS.contains(c))
+            .map(|c| MORSE[LETTERS.iter().position(|x| *x == c).unwrap()])
+            .fold(String::new(), |mut acc, x| {
+                acc.push_str(x);
+                acc.push(' ');
+                acc
+            })
+            .trim()
+            .to_string()
+    }
+    fn decrypt(&self, _language: &Language, msg: &String) -> String {
+        msg.split('/')
+            .map(|word| {
+                word.replace('_', "-")
+                    .trim()
+                    .split(' ')
+                    .map(|letter| MORSE.iter().position(|&x| x == letter))
+                    .filter(|&x| x != None)
+                    .map(|pos| LETTERS[pos.unwrap()])
+                    .fold(String::new(), |mut acc, x| {
+                        acc.push(x);
+                        acc
+                    })
+            })
+            .fold(String::new(), |mut acc, x| {
+                acc.push_str(&x);
+                acc.push(' ');
+                acc
+            })
+            .trim()
+            .to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn encrypt() {
+        let msg = String::from("morse code");
+        let language = Language::new();
+        let morse = Morse{};
+        let encrypted = morse.encrypt(&language, &msg);
+        assert_eq!(encrypted, "-- --- .-. ... . / -.-. --- -.. .");
+    }
+
+    #[test]
+    fn decrypt() {
+        let msg = String::from("-- --- .-. ... . / -.-. --- -.. .");
+        let language = Language::new();
+        let morse = Morse{};
+        let decrypted = morse.decrypt(&language, &msg);
+        assert_eq!(decrypted, "morse code");
+    }
+}
