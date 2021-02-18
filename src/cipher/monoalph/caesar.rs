7d0e0e0e46179f06a8c5c270f193747c10ba33c2
Tom Thorogood <tomthorogood@outlook.com>
2021-02-18 17:45:08 +0000

    Implemented Caesar cipher

---
+++ src/cipher/monoalph/caesar.rs
@@ -0,0 +1,55 @@
+use crate::cipher::cipher::{Asymmetric, Keyed};
+use crate::lang::Language;
+use crate::util;
+
+pub struct Caesar {
+    shift: i16
+}
+
+impl Asymmetric for Caesar {
+    fn encrypt(&self, language: &Language, msg: &String) -> String {
+        msg
+            .chars()
+            .map(|c| {
+                if language.is_letter(&c) {
+                    let mut cp = language.get_cp(&c);
+                    cp = util::modulo(cp + self.shift, language.cp_count());
+                    language.update_cp(&c, cp)
+                } else {
+                    c
+                }
+            })
+            .collect()
+    }
+    fn decrypt(&self, language: &Language, msg: &String) -> String {
+        msg
+            .chars()
+            .map(|c| {
+                if language.is_letter(&c) {
+                    let mut cp = language.get_cp(&c);
+                    cp = util::modulo(cp - self.shift, language.cp_count());
+                    language.update_cp(&c, cp)
+                } else {
+                    c
+                }
+            })
+            .collect()
+    }
+}
+
+impl Keyed for Caesar {
+    fn new() -> Caesar {
+        Caesar {
+            shift: 0
+        }
+    }
+    fn reset(&mut self, _language: &Language) {
+        self.shift = 0;
+    }
+    fn randomize(&mut self, language: &Language, rng: &mut impl rand::Rng) {
+        self.shift = rng.gen_range(0..language.cp_count());
+    }
+    fn to_string(&self, _language: &Language) -> String {
+        format!("shift:{}", self.shift)
+    }
+}
