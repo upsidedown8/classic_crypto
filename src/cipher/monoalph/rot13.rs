d02c225151d8d010a835fbba263da8290782b4bc
Tom Thorogood <tomthorogood@outlook.com>
2021-02-18 19:13:44 +0000

    Implemented ROT13

---
+++ src/cipher/monoalph/rot13.rs
@@ -0,0 +1,22 @@
+use crate::cipher::cipher::Symmetric;
+use crate::lang::Language;
+
+pub struct ROT13 {}
+
+impl Symmetric for ROT13 {
+    fn run(&self, language: &Language, msg: &String) -> String {
+        assert_eq!(language.cp_count()%2, 0);
+        msg
+            .chars()
+            .map(|c| {
+                if language.is_letter(&c) {
+                    let mut cp = language.get_cp(&c);
+                    cp = language.cp_count()/2 - cp;
+                    language.update_cp(&c, cp)
+                } else {
+                    c
+                }
+            })
+            .collect()
+    }
+}
