81f7082081443e766f8928e79731e7ed25b0fa7c
Tom Thorogood <tomthorogood@outlook.com>
2021-02-18 16:59:52 +0000

    Implemented Atbash cipher

---
+++ src/cipher/monoalph/atbash.rs
@@ -0,0 +1,21 @@
+use crate::cipher::cipher::Symmetric;
+use crate::lang::Language;
+
+pub struct Atbash {}
+
+impl Symmetric for Atbash {
+    fn run(&self, language: &Language, msg: &String) -> String {
+        msg
+            .chars()
+            .map(|c| {
+                if language.is_letter(&c) {
+                    let mut cp = language.get_cp(&c);
+                    cp = language.cp_count() - 1 - cp;
+                    language.update_cp(&c, cp)
+                } else {
+                    c
+                }
+            })
+            .collect()
+    }
+}
