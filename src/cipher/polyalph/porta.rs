7730ffa617db5d5409dba89b932cece754516d79
Tom Thorogood <tomthorogood@outlook.com>
2021-02-20 00:59:21 +0000

    Implemented Porta cipher

---
+++ src/cipher/polyalph/porta.rs
@@ -0,0 +1,54 @@
+use crate::key::{keyword::Keyword, vigenere_square::vig_square::VigSquare};
+use crate::lang::Language;
+use crate::{
+    cipher::cipher::{Symmetric, Keyed},
+    key::{
+        key::{Key, StatefulKey},
+        vigenere_square::porta_square::PortaSquare,
+    },
+};
+
+pub struct Porta {
+    pub square: PortaSquare,
+    pub keyword: Keyword,
+}
+
+impl Porta {}
+
+impl Symmetric for Porta {
+    fn run(&self, language: &Language, msg: &String) -> String {
+        let mut count = 0;
+        msg.chars()
+            .map(|c| {
+                if language.is_letter(&c) {
+                    let new_cp = self.square.encrypt(
+                        self.keyword.at(count % self.keyword.len()) / 2,
+                        language.get_cp(&c)
+                    );
+                    count += 1;
+                    language.update_cp(&c, new_cp)
+                } else {
+                    c
+                }
+            })
+            .collect()
+    }
+}
+
+impl Keyed for Porta {
+    fn new(language: &Language) -> Porta {
+        Porta {
+            square: PortaSquare::new(language),
+            keyword: Keyword::new(language),
+        }
+    }
+    fn reset(&mut self, language: &Language) {
+        self.keyword.reset(language);
+    }
+    fn randomize(&mut self, language: &Language, rng: &mut impl rand::Rng) {
+        self.keyword.randomize(language, rng);
+    }
+    fn to_string(&self, language: &Language) -> String {
+        format!("Keyword:{}", self.keyword.to_string(language))
+    }
+}
