d46c452fef3515aba0ecc87a717dc91361b873c7
Tom Thorogood <tomthorogood@outlook.com>
2021-02-22 17:09:42 +0000

    Cargo fmt --all

--- src/key/mod.rs
+++ src/key/mod.rs
@@ -10,21 +10,20 @@ mod polybius_square;
 mod straddle_checkerboard;
 mod vigenere_square;
 
+pub use alphabet::Alphabet;
+pub use cards::Cards;
 pub use enigma::plugboard::Plugboard;
 pub use enigma::reflector::*;
 pub use enigma::rotor::*;
+pub use keyword::Keyword;
+pub use matrix::Matrix;
 pub use vigenere_square::bellaso_square::BellasoSquare;
 pub use vigenere_square::classic_vig_square::ClassicVigSquare;
 pub use vigenere_square::keyed_vig_square::KeyedVigSquare;
 pub use vigenere_square::porta_square::PortaSquare;
 pub use vigenere_square::VigSquare;
-pub use alphabet::Alphabet;
-pub use cards::Cards;
-pub use keyword::Keyword;
-pub use matrix::Matrix;
 // pub use polybius_square::PolybiusSquare;
 // pub use straddle_checkerboard::StraddleCheckerboard;
-
 
 pub trait KeyFrom<T> {
     fn create_from(language: &Language, _: T) -> Self;
