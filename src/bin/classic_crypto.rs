1e07de7d0b873bf865626bd7f32e4bc6b52a3094
Tom Thorogood <tomthorogood@outlook.com>
2021-05-10 23:54:31 +0100

    Removed example from main

--- src/bin/classic_crypto.rs
+++ src/bin/classic_crypto.rs
@@ -1,18 +1,1 @@
-use classic_crypto::{Asymmetric, BlockTransposition, Keyed, Solve, key::SetKey, lang::Language};
-
-fn main() {
-    let mut language = Language::from_file("examples/data/english.bin").unwrap();
-    let mut cipher = BlockTransposition::new(&mut language);
-    
-    let key = language.string_to_vec("badc");
-    cipher.keyword.set_key(&mut language, &key);
-    
-    let plaintext = "Abc d a BC d abcd abcd";
-
-    let ciphertext = cipher.encrypt(&mut language, plaintext);
-    let plaintext = cipher.decrypt(&mut language, &ciphertext);
-
-    println!("ciphertext: {}", ciphertext);
-    println!("plaintext: {}", plaintext);
-    println!("cipher: {}", cipher.to_string(&mut language));
-}
+fn main() {}
